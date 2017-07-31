576485f1e83bc4f0c617df7218b6bd9c4cd5f036
AnnaEingor <lermananna2@gmail.com>
2017-07-31 15:17:48 +0300

    menuGallery work

--- orders/orderFood/menuGalleryCtrl.js
+++ orders/orderFood/menuGalleryCtrl.js
@@ -1,17 +1,9 @@
 orderApp.controller("menuGalleryCtrl", function($scope, $http, $location, menus){
-  
-    if(menus.getAllList().length===0){
-            $scope.orderArr = [];
-            $http.get("data/menuTA.json").then(function(response){
-            menus.load(response.data);
-            $scope.orderArr = menus.getAllList;
-            })
-         }
-         else{
-            $scope.orderArr = menus.getAllList;
-         }
-        
-      
+    $scope.orderArr = [];
+    $http.get("data/menuTA.json").then(function(response){
+       $scope.orderArr = response.data;
+    });
+
         $scope.openDetails = function(index) {
         $location.path("/menus/" + index)
     }
