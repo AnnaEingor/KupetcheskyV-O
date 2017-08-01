ec326ade21f241ae5b72241617294b445422833f
AnnaEingor <lermananna2@gmail.com>
2017-08-01 15:41:25 +0300

    table.html and tableCtrl.js are work

--- orders/orderTable/tableCtrl.js
+++ orders/orderTable/tableCtrl.js
@@ -5,25 +5,42 @@ orderApp.controller("tableCtrl", function($scope){
    $scope.clientName = "Name";
    $scope.quantity = "";
 
+ $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
    $scope.clear = function() {
     $scope.mytime = null;
     $scope.dt = null;
   };
 
-  $scope.today = function() {
-    $scope.dt = new Date();
+ $scope.inlineOptions = {
+    customClass: getDayClass,
+    minDate: new Date(),
+    showWeeks: false
   };
-  $scope.today();
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    maxDate: new Date(2029, 7, 09),
+    minDate: new Date(),
+    startingDay: 1
+  };
+
+  
+  $scope.toggleMin = function() {
+    $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+    $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+  };
+
+  $scope.toggleMin();
 
   $scope.open1 = function() {
     $scope.popup1.opened = true;
   };
 
-   $scope.popup1 = {
-    opened: false
-  };
-
-  $scope.setDate = function(year, month, day) {
+   $scope.setDate = function(year, month, day) {
     $scope.dt = new Date(year, month, day);
   };
 
@@ -31,14 +48,42 @@ orderApp.controller("tableCtrl", function($scope){
   $scope.format = $scope.formats[0];
   $scope.altInputFormats = ['M!/d!/yyyy'];
 
-  
-
- $scope.dateOptions = {
-    dateDisabled: disabled,
-    formatYear: 'yy',
-    maxDate: new Date(2025, 7, 09),
-    minDate: new Date(),
-    startingDay: 1
+  $scope.popup1 = {
+    opened: false
   };
 
+  
+  var tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  var afterTomorrow = new Date();
+  afterTomorrow.setDate(tomorrow.getDate() + 1);
+  $scope.events = [
+    {
+      date: tomorrow,
+      status: 'full'
+    },
+    {
+      date: afterTomorrow,
+      status: 'partially'
+    }
+  ];
+
+  function getDayClass(data) {
+    var date = data.date,
+      mode = data.mode;
+    if (mode === 'day') {
+      var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+      for (var i = 0; i < $scope.events.length; i++) {
+        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+        if (dayToCheck === currentDay) {
+          return $scope.events[i].status;
+        }
+      }
+    }
+
+    return '';
+  }
 });
+
