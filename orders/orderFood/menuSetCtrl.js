879dda86b2111489e7db497f977a402dba1b2653
AnnaEingor <lermananna2@gmail.com>
2017-08-05 20:35:56 +0300

    nothing

--- orders/orderFood/menuSetCtrl.js
+++ orders/orderFood/menuSetCtrl.js
@@ -1,16 +1,16 @@
 
-orderApp.controller("menuSetCtrl", function($scope, $routeParams, menus, MenuTA){
+// orderApp.controller("menuSetCtrl", function($scope, $routeParams, menus, MenuTA){
  
 
-   if(menus.getAllList().length===0){
-    $scope.orderArr = menus.getAllList();
-}
+//    if(menus.getAllList().length===0){
+//     $scope.orderArr = menus.getAllList();
+// }
 
-   $scope.order = menus.getPosMenu($routeParams.orderIndex);
-   $scope.quantityOfPosition = "0";
+//    $scope.order = menus.getPosMenu($routeParams.orderIndex);
+//    $scope.quantityOfPosition = "0";
 
-   $scope.addToOrder  = function(){
-       
-   }
+//    $scope.addToOrder  = function(){
+
+//    }
     
-});
+// });
