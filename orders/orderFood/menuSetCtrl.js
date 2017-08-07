44f42eabcb07afadce8a0493a6b1d65c278774ca
AnnaEingor <lermananna2@gmail.com>
2017-08-07 22:38:04 +0300

    added rating

--- orders/orderFood/menuSetCtrl.js
+++ orders/orderFood/menuSetCtrl.js
@@ -1,5 +1,10 @@
 
 orderApp.controller("menuSetCtrl", function($scope,$http,menus){
-      
+       $scope.rate = 7;
+       $scope.max = 10;
+     $scope.isReadonly = false;
 
+  $scope.ratingStates = [
+    {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+   ];
  });
