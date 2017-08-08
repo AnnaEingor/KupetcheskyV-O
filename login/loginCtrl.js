e422b0d6177ae73ae5a399418b88d752a1b511e3
AnnaEingor <lermananna2@gmail.com>
2017-08-08 12:07:58 +0300

    map work

--- login/loginCtrl.js
+++ login/loginCtrl.js
@@ -1,9 +1,24 @@
-// orderApp.controller("LoginCtrl", function ($scope, $uibModalInstance, $http, $location, activeUser, User) {
+ orderApp.controller("LoginCtrl", function ($scope, $uibModalInstance, $http, $location, activeUser, User) {
 
-//     $scope.login = function() {
+    $http.get("app/data/users.json").then(function (response) {
+        $scope.users = [];
+        for (var i = 0; i < response.data.length; i++) {
+            $scope.users.push(new User(response.data[i]));
+        }
 
-//         $uibModal.open({
-//             templateUrl: "/login/loginCtrl.html",
-//             controller: "LoginCtrl"
-//         })
-//     }
+        //console.log(JSON.stringify($scope.users));
+    });
+
+    $scope.failedAttempt = false;
+
+    $scope.login = function() {
+        var user = getLoggedInUser();
+        if (user != null) {
+            activeUser.login(user);
+            $uibModalInstance.close("Logged-in");
+            $location.path("/payment");
+        } else {
+            $scope.failedAttempt = true;
+        }
+    };
+ });        
--- orders/orderFood/delivery/deliveryConfirmCtrl.js
+++ orders/orderFood/delivery/deliveryConfirmCtrl.js
@@ -1,25 +1,33 @@
 
-// function initMap(){
+ function initMap(){
    
-//     var centerLatLng = new google.maps.LatLng(56.2928515, 43.7866641);
+     var centerLatLng = new google.maps.LatLng(56.2928515, 43.7866641);
  
    
-//     var mapOptions = {
-//         center: centerLatLng, 
-//         zoom: 8               
-//     };
+     var mapOptions = {
+         center: centerLatLng, 
+         zoom: 9               
+     };
  
     
-//     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-// }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ }
  
-// google.maps.event.addDomListener(window, "load", initMap);
+ google.maps.event.addDomListener(window, "load", initMap);
 
 
 
-orderApp.controller("deliveryConfirmCtrl", function($scope){
+orderApp.controller("deliveryConfirmCtrl", function($scope, $uibModal){
 
+    $scope.goToLogin = function() {
+        $uibModal.open({
+            templateUrl: "login.html",
+            controller: "LoginCtrl"
+        })
+    };
 
 
 });
+
+
 
