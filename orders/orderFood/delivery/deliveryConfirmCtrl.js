190823a6d1cebec6eeb1293481e7b9a690f4b6f3
AnnaEingor <lermananna2@gmail.com>
2017-08-10 11:22:21 +0300

    map works

--- orders/orderFood/delivery/deliveryConfirmCtrl.js
+++ orders/orderFood/delivery/deliveryConfirmCtrl.js
@@ -1,26 +1,26 @@
 
-//   function initMap(){
-//      var centerLatLng = new google.maps.LatLng(56.2928515, 43.7866641);
-//      var mapOptions = {
-//          center: centerLatLng, 
-//          zoom: 9               
-//     };
-//     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-//     }
-//    google.maps.event.addDomListener(window, "load", initMap);
-
-
-
- orderApp.controller("deliveryConfirmCtrl", function($scope, $location, orderService){
+  orderApp.controller("deliveryConfirmCtrl", function($scope, $location, orderService){
     
     $scope.orders = orderService.getAllOrders();
     $scope.total = orderService.total();
-
-      
-     //to do delivery. Must be = formula(distance from restaurant to address from placeID(Google place autocomplete)
+     
+    
      $scope.goToPayment = function () {
         $location.path("/login");
 }
+
+ //to do delivery. Must be = formula(distance from restaurant to address from placeID(Google place autocomplete)
+
+$scope.initMap = function(){
+     var centerLatLng = new google.maps.LatLng(56.2928515, 43.7866641);
+    var mapOptions = {
+         center: centerLatLng, 
+         zoom: 9               
+     };
+     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    }
+    google.maps.event.addDomListener(window, "load", $scope.initMap);
+
  });
 
 
