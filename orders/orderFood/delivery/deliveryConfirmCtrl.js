402e60b7d329d89ee375500a32562d79fa7a9675
AnnaEingor <lermananna2@gmail.com>
2017-08-13 18:56:01 +0300

    created mapService

--- orders/orderFood/delivery/deliveryConfirmCtrl.js
+++ orders/orderFood/delivery/deliveryConfirmCtrl.js
@@ -1,5 +1,5 @@
 
-  orderApp.controller("deliveryConfirmCtrl", function($scope, $location, orderService){
+  orderApp.controller("deliveryConfirmCtrl", function($scope, $location, orderService, mapService){
     
     $scope.orders = orderService.getAllOrders();
     $scope.total = orderService.total();
@@ -9,23 +9,15 @@
         $location.path("/login");
 }
 
- //to do delivery. Must be = formula(distance from restaurant to address from placeID(Google place autocomplete)
+  google.maps.event.addDomListener(window, "load", mapService.initMap);
+  
+  $scope.address = mapService.getAddress();
+ 
+  $scope.delivery = mapService.delivery();
+      
 
-$scope.initMap = function(){
-     var centerLatLng = new google.maps.LatLng(56.329549, 44.026211);
-    var mapOptions = {
-         center: centerLatLng, 
-         zoom: 9               
-     };
-     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-      var marker = new google.maps.Marker({
-     position: centerLatLng,
-     map: map
   });
-    }
-    google.maps.event.addDomListener(window, "load", $scope.initMap);
 
- });
 
 
 
---
+++ services/mapService.js
@@ -0,0 +1,57 @@
+orderApp.factory("mapService", function () {
+var initMap = function initMap() {
+    var centerLatLng = new google.maps.LatLng(56.329549, 44.026211);
+    var mapOptions = {
+         center: centerLatLng, 
+         zoom: 12               
+     };
+     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+     var marker = new google.maps.Marker({
+     position: centerLatLng,
+     map: map,
+     title:"Restaurant Kupetchesky"
+  });
+        
+}
+
+var getAddress = function (){
+    return address;
+};
+
+ var distance = function distance(){
+ var autocomplete = new google.maps.places.Autocomplete(address);
+ 
+  google.maps.event.addListener(autocomplete, 'place_changed', function () {
+     var place = autocomplete.getPlace();
+     
+     var addressLat = place.geometry.location.lat();
+      var addressLng = place.geometry.location.lng();
+     var dist = google.maps.geometry.spherical.computeDistanceBetween(google.maps.LatLng(56.329549, 44.026211), 
+           new google.maps.LatLng(addressLat, addressLng));
+           });  
+      return dist;
+ };
+
+  var delivery = function delivery(dist){
+   if(dist < 3000){
+       deliv = 500;
+   }
+     else if(dist>=3000 && dist<7000){
+         deliv = 700;
+     }
+     else {dist > 7000}{
+        deliv = 1000;
+     }
+        return deliv;
+ }
+
+return{
+    initMap: initMap,
+    getAddress: getAddress,
+    distance: distance,
+    delivery: delivery
+   
+}
+});
+
